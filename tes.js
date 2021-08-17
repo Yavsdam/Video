f3c1594cf2f6d86028a680ccc664cecb1683be01
Yavsdam <87897115+Yavsdam@users.noreply.github.com>
2021-08-17 20:18:13 +0300

    Create tes.js

---
+++ tes.js
@@ -0,0 +1,4 @@
+alert("bu kod lin olmadan tasarlanmıştır");
+alert("evet öyle");
+var test = prompt("mesela bir örnek yapalım sen buraya yazdığın şeyle google da arıyıcak test edelim mi?")
+window.location.href = ("https://google.coö/search?q="+test);
