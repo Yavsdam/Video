443a39abba7105ec2786255cecc94a0e3702558f
Yavsdam <87897115+Yavsdam@users.noreply.github.com>
2021-08-17 20:22:44 +0300

    Update tes.js

--- tes.js
+++ tes.js
@@ -1,4 +1,2 @@
 alert("bu kod lin olmadan tasarlanmıştır");
-alert("evet öyle");
-var test = prompt("mesela bir örnek yapalım sen buraya yazdığın şeyle google da arıyıcak test edelim mi?")
-window.location.href = ("https://google.coö/search?q="+test);
+
